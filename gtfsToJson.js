4284c7474bfa0c7060f6df3216b5e7ca4ae26e6d
ThisTemba <tembamateke@gmail.com>
2022-10-13 08:57:31 -0400

    Add main.js and gtfsToJson to unzip and parse

---
+++ gtfsToJson.js
@@ -0,0 +1,135 @@
+const fs = require("fs");
+const { mainModule } = require("process");
+
+const GTFS_SPEC_FILENAMES = [
+  // fetched on 2022-10-13 from https://developers.google.com/transit/gtfs/reference#table_of_contents
+  "agency.txt",
+  "stops.txt",
+  "routes.txt",
+  "trips.txt",
+  "stop_times.txt",
+  "calendar.txt",
+  "calendar_dates.txt",
+  "feed_info.txt",
+  "fare_attributes.txt",
+  "fare_rules.txt",
+  "shapes.txt",
+  "frequencies.txt",
+  "transfers.txt",
+  "pathways.txt",
+  "levels.txt",
+  "translations.txt",
+  "attributions.txt",
+];
+
+function readTxt(path) {
+  return fs.readFileSync(path, "utf8");
+}
+
+function stringify(value) {
+  try {
+    return JSON.stringify(value, null, 2); // this is nicely formatted with line breaks
+  } catch (err) {
+    return JSON.stringify(value); // this is a single line
+  }
+}
+
+function writeJSON(path, data) {
+  const dataString = stringify(data);
+  fs.writeFileSync(path, dataString);
+}
+
+function removeCharsFromString(string, chars) {
+  if (!string) return "";
+  for (const char of chars) {
+    string = string.replace(char, "");
+  }
+  return string;
+}
+
+function getLines(filename) {
+  const charsToRemove = ["\r", '"'];
+  let dataString = readTxt(filename);
+  dataString = removeCharsFromString(dataString, charsToRemove);
+  const lines = dataString.split("\n");
+  return lines;
+}
+
+async function getCollection(path) {
+  // a collection is an array of objects
+  const lines = getLines(path);
+  const firstLine = lines.shift();
+  const headers = firstLine.split(",");
+  const collection = lines.map((line) => {
+    const row = {};
+    const values = line.split(",");
+    values.forEach((value, index) => {
+      const key = headers[index];
+      row[key] = value;
+    });
+    return row;
+  });
+  return collection;
+}
+
+// files to convert is all txt files in the source dir that are in the gtfs spec
+function getFilenamesToConvert(dir, specFilenames) {
+  // check that dir exists
+  if (!fs.existsSync(dir)) {
+    console.error(`Directory ${dir} does not exist`);
+    throw new Error("Directory does not exist");
+  }
+
+  const filenames = fs.readdirSync(dir);
+  const txtFilenames = filenames.filter((fname) => fname.endsWith(".txt"));
+  const filesToConvert = txtFilenames.filter((fname) =>
+    specFilenames.includes(fname)
+  );
+  return filesToConvert.map((filename) => `${dir}/${filename}`);
+}
+
+async function convertFile(path, sourceDir, targetDir) {
+  const filename = path.split("/").pop();
+  console.log(`Converting ${filename}`);
+  try {
+    const collection = await getCollection(path);
+    if (collection.length > 0) {
+      writeJSON(
+        path.replace(".txt", ".json").replace(sourceDir, targetDir),
+        collection
+      );
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//TODO: maybe also sort the shapes file by point sequence
+
+async function convert(sourceDir, targetDir) {
+  console.log("Converting files in " + sourceDir);
+  const filenamesToConvert = getFilenamesToConvert(
+    sourceDir,
+    GTFS_SPEC_FILENAMES
+  );
+  // if target dir doesn't exist, create it
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir);
+  }
+  for (const filename of filenamesToConvert) {
+    await convertFile(filename, sourceDir, targetDir);
+  }
+}
+
+function main() {
+  const agencyFolderName = "mbtaGtfs";
+  const sourceDir = `data/extracted/${agencyFolderName}`;
+  const targetDir = `data/parsed/${agencyFolderName}`;
+  convert(sourceDir, targetDir);
+}
+
+// export as node module
+
+module.exports = {
+  convert,
+};
---
+++ main.js
@@ -0,0 +1,39 @@
+// 1. Extract from zip
+// 2. Convert to JSON
+// 3. Process any files that need to be processed
+// 4. save the result in data/parsed
+
+const extract = require("extract-zip");
+const fs = require("fs");
+const gtfsToJson = require("./gtfsToJson");
+
+const RAW_PATH = "data/raw";
+const EXTRACTED_PATH = "data/extracted";
+const PARSED_PATH = "data/parsed";
+
+async function unzipAll(rawPath, extractedPath) {
+  const filenames = fs.readdirSync(rawPath);
+  const dir = __dirname;
+  for (const filename of filenames) {
+    console.log(`Unzipping ${filename}`);
+    const zipPath = `${dir}/${rawPath}/${filename}`;
+    const extractPath = `${dir}/${extractedPath}/${filename}`.replace(
+      ".zip",
+      ""
+    );
+    await extract(zipPath, { dir: extractPath });
+  }
+}
+
+async function main() {
+  await unzipAll(RAW_PATH, EXTRACTED_PATH);
+  const agencies = fs.readdirSync(EXTRACTED_PATH);
+  for (const agency of agencies) {
+    const sourceDir = `${EXTRACTED_PATH}/${agency}`;
+    const targetDir = `${PARSED_PATH}/${agency}`;
+    await gtfsToJson.convert(sourceDir, targetDir);
+  }
+  // rename all folders in extracted
+}
+
+main();
